8f2a0b8d3dfb0a425de05b9477298b2aced91eed
dorianzheng <xingzhengde72@gmail.com>
2025-11-17 10:29:19 +0800

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
-  integrations: [mdx(), sitemap(), icon()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: 'https://polygala-cloud.github.io',
+  base: '/website',
 });
